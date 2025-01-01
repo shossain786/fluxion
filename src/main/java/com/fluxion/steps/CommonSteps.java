8fe87edaf046d5c6a4839dbb047ce253f1e1455d
Mohammad Saddam Hossain <sddmhossain786@gmail.com>
2025-01-01 21:25:29 +0530

    code updated

--- src/main/java/com/fluxion/steps/CommonSteps.java
+++ src/main/java/com/fluxion/steps/CommonSteps.java
@@ -1,4 +1,23 @@
 package com.fluxion.steps;
 
+import com.fluxion.core.DriverManager;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
 public class CommonSteps {
+    @Given("I open the login page")
+    public void iOpenTheLoginPage() {
+        DriverManager.getDriver().get("https://google.com/login");
+    }
+
+    @Then("I enter username and password")
+    public void iEnterUsernameAndPassword() {
+        System.out.println("Title of the page: " + DriverManager.getDriver().getTitle());
+    }
+
+    @And("I close the browser")
+    public void iCloseTheBrowser() {
+        DriverManager.quitDriver();
+    }
 }
